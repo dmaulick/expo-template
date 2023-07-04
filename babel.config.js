c226546225557186d899099a47d0f8757e5c3a79
David Maulick <david.maulick@coinbase.com>
2023-07-04 17:55:57 -0400

    router works in src directory.

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,18 @@
+// This is referenced in the context of node_modules/expo-router/entry.js, so the path to src is rooted there (note this would change in a monorepo)
+process.env.EXPO_ROUTER_APP_ROOT="../../src/app"
+
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
     plugins: [
       require.resolve("expo-router/babel"),
+      [
+        'transform-inline-environment-variables',
+        {
+          include: ['EXPO_ROUTER_APP_ROOT'],
+        },
+      ],
     ],
   };
 };
